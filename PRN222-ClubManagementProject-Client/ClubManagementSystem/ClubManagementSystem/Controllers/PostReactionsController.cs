5a70ab6c47c4d9706d9c541c68f42f19aff4986e
Mai Xuân Hiếu <maixuanhieu250103@gmail.com>
2025-04-18 14:13:08 +0700

    [FEAT] Implement Post & Post Reaction feature

---
+++ PRN222-ClubManagementProject-Client/ClubManagementSystem/ClubManagementSystem/Controllers/PostReactionsController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BussinessObjects.Models;
+using Services.Interface;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using ClubManagementSystem.Controllers.SignalR;
+
+namespace ClubManagementSystem.Controllers
+{
+    [Authorize]
+    public class PostReactionsController : Controller
+    {
+        private readonly IPostReactionService _reactionService;
+        private readonly IPostService _postService;
+        private readonly SignalRSender _sender;
+        public PostReactionsController(IPostReactionService postReactionService, IPostService postService, SignalRSender sender)
+        {
+            _reactionService = postReactionService;
+            _postService = postService;
+            _sender = sender;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ToggleLike(int postId)
+        {
+            var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? "0");
+
+            int likeCount = await _reactionService.ToggleReactionAsync(postId, userId);
+            //_sender.NotifyPost(null, postId)
+
+            return Json(new { success = true, likeCount });
+        }
+    }
+}
---
+++ PRN222-ClubManagementProject-Client/ClubManagementSystem/Services/Interface/IPostService.cs
@@ -0,0 +1,26 @@
+﻿using BussinessObjects.Models;
+using BussinessObjects.Models.Dtos;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interface
+{
+    public interface IPostService
+    {
+        Task<Post> CreatePostAsync(Post model, IFormFile? imageFile, int userId, int clubId);
+        Task<IEnumerable<Post>> GetPostsAsync(int clubId);
+        Task<IEnumerable<Post>> GetPostsAsync(int clubId, string status);
+        Task<IEnumerable<Post>> GetPostsAsync(string status);
+        Task<IEnumerable<Post>> GetPostsAsync();
+        Task<PostDetailsDto?> GetPostDetailsAsync(int postId);
+        Task UpdatePostAsync(PostUpdateDto postDto);
+        Task UpdatePostAsync(Post post);
+        Task<Post> GetPostAsync(int postId);
+        Task DeletePostAsync(int postId);
+
+    }
+}
