88d9a5b154cecc529f0d80d23cad2676f00fa996
DevHuy44 <kieuvanhuy813@gmail.com>
2025-04-21 21:55:51 +0700

    complete function manage Fee Member

---
+++ PRN222-ClubManagementProject-Admin/ClubManagementSystem/BussinessObjects/Models/Fee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessObjects.Models
+{
+    public class Fee
+    {
+        [Key]
+        public int FeeId { get; set; }
+
+        [Required]
+        public int ClubId { get; set; }
+
+        [Required]
+        public string FeeDescription { get; set; } // Mô tả đợt phí (VD: "Phí thành viên năm 2025")
+
+        [Required]
+        public string FeeType { get; set; } // Membership, Event, Penalty
+
+        [Required]
+        public string PaymentMethod { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public DateTime DueDate { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+
+        // Navigation property
+        [ForeignKey("ClubId")]
+        public Club Club { get; set; }
+
+        // Quan hệ với MembershipFee (một Fee có nhiều MembershipFee)
+        public ICollection<MembershipFee> MembershipFees { get; set; }
+    }
+}
---
+++ PRN222-ClubManagementProject-Admin/ClubManagementSystem/Repositories/Interface/IMembershipFeeRepository.cs
@@ -0,0 +1,28 @@
+﻿using BussinessObjects.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Interface
+{
+    public interface IMembershipFeeRepository
+    {
+        Task<List<ClubMember>> GetClubMembersAsync(int clubId);
+        Task<Club> GetClubByIdAsync(int clubId);
+        Task<Fee> AddFeeAsync(Fee fee);
+        Task AddMembershipFeeAsync(MembershipFee membershipFee);
+        Task AddNotificationAsync(Notification notification);
+        Task UpdateMembershipFeeAsync(MembershipFee membershipFee);
+        Task<List<Fee>> GetFeesByClubAsync(int clubId);
+        Task<Fee> GetFeeByIdAsync(int feeId);
+        Task<List<MembershipFee>> GetMembershipFeesByFeeIdAsync(int feeId);
+        Task DeleteFeeAndRelatedFeesAsync(int feeId);
+        Task UpdateFeeAsync(Fee fee);
+        Task<List<MembershipFee>> GetMembershipFeesByUserIdAsync(int userId);
+        Task<MembershipFee> GetMembershipFeeByIdAsync(int membershipFeeId, int userId);
+        // Phương thức mới
+        Task<List<MembershipFee>> GetMembershipFeesByDueDateAsync(DateTime dueDate, string status);
+    }
+}
